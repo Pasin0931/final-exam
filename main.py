0d9500fca5d3e9d9a1f946eee1245bb9754f9943
Pasin <pasin0931@gmail.com>
2025-12-04 15:07:53 +0700

    Add Person, Customer, Driver classes

---
+++ main.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        return f"Hi, I'm {self.name}"
+            
+class Customer(Person):
+    def __init__(self, name, address):
+        super().__init__(name)
+        self.address = address
+
+    def place_order(self, item):
+        return item
+    
+class Driver(Person):
+    def __init__(self, name, vehicle):
+        super().__init__(name)
+        self.vehicle = vehicle
+
+    def deliver(self, other, order):
+        print(f"{self.name} is delivering {order} to {other.name} using {self.vehicle}")
+        
